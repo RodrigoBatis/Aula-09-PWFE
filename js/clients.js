f5db9f8c44fe0db4b7245cc9ec71b759ab331f80
RodrigoBatis <rodrigofreirebatista@gmail.com>
2022-05-04 16:50:50 -0300

    <>

--- js/clients.js
+++ js/clients.js
@@ -1,34 +1,73 @@
-"use strict"
+'use strict'
 
-const url = "https://testeleonid.herokuapp.com/clientes"
+// Url Base
+ // const url = 'https://testeleonid.herokuapp.com/'
+  const url = 'http://localhost/rodrigo/cliente/'
 
-const readClients = async () => {
-    const response = await fetch(url)
-    return await response.json()
-}
 
+/**
+ * Função que encaminha os dados do usuário para a API
+ * @param {JSON} client - Objeto que contém todas as informações do cliente
+ * @returns {Boolean}
+ */
 const createClient = async (client) => {
     const options = {
-        'method': 'POST',
-        'body': JSON.stringify(client),
-        'headers' : {
-            'content-type' : 'application/json'
+        method: 'POST',
+        body: JSON.stringify(client),
+        headers: {
+            'content-type': 'application/json'
         }
     }
 
-    const response = await fetch(url, options)
+    const response = await fetch(`${url}clientes`, options)
+    
+    if(response.ok)
+        alert('Cliente cadastrado com sucesso!')
+}
+
+/**
+ * Função que retorna todos os usuários cadastrados
+ * @param {String} id ID do Cliente para busca
+ * @returns {JSON} Retorna um JSON com os dados de todos os usuários
+ */
+const readClients = async (id = '') => {
+    const response = await fetch(`${url}clientes/${id}`)
+    const data = await response.json()
+
+    return data
+}
+
+// const readClient = async (idClient) => {
+//     const response = await fetch(`${url}clientes/${idClient}`)
+//     const data = response.json()
+
+//     return data
+// }
+
+const updateClient = async (client) => {
+    const options = {
+        method: 'PUT',
+        body: JSON.stringify(client),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }
+    const response = await fetch(`${url}clientes/${client.id}`, options)
+    console.log('UPDATE: ', response.ok)
+}
+
+/**
+ * Função que excluí um cliente
+ * @param {Number} idClient - ID do cliente a ser excluído
+ * @returns {Boolean}
+ */
+const deleteClient = async (idClient) => {
+    const options = {
+        method: 'DELETE',
+    }
+
+    const response = await fetch(`${url}clientes/${idClient}`, options)
     console.log(response.ok)
 }
 
-const deleteClient = async (codigo) => {
-    const options = {
-        'method': 'DELETE'
-    }
-    const response = await fetch(`${url}/${codigo}`, options)
-}
-
-export{
-    readClients,
-    createClient,
-    deleteClient
-}
+export { readClients, createClient, deleteClient, updateClient }
--- js/modal.js
+++ js/modal.js
@@ -2,13 +2,17 @@
 
 const openModal = () => document.getElementById('modal').classList.add('active')
 
-const closeModal = () => document.getElementById('modal').classList.remove('active')
+const closeModal = () => {
+    document.getElementById('modal').classList.remove('active');
+    
+    // Limpando os dados
+    document.getElementById('modal-form').reset() // Limpa todos os campos de um formulário
+    document.getElementById('txtNome').removeAttribute('data-client-id')
+}
 
 document.getElementById('modalClose').addEventListener('click', closeModal)
 
 document.getElementById('cancelar').addEventListener('click', closeModal)
 
-export{
-    openModal,
-    closeModal
-}
+// Export 
+export { openModal, closeModal }
